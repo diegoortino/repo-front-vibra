eeb0c630fea6e0f80ce6ec4348c0f40bd049cfe1
SergioPeck <sergiopeckerle2004@gmail.com>
2025-09-24 14:04:43 -0300

    Cambios que quiero pasar a feature/Profile

--- vibraApp/src/components/Main.tsx
+++ vibraApp/src/components/Main.tsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Profile from "./UserPage/Profile"
+
 export function Main(){
-    return null
+    return(
+        <div>
+            <Routes>
+                <Route path="/account" element={<Profile/>} />
+            </Routes>
+        </div>
+
+    )
 }
