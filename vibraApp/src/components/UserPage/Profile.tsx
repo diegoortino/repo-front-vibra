efae134df9c7bd3be716939d491ecef5cc5c84ba
SergioPeck <sergiopeckerle2004@gmail.com>
2025-10-08 18:08:29 -0300

    ProfileSkeleton

--- vibraApp/src/components/UserPage/Profile.tsx
+++ vibraApp/src/components/UserPage/Profile.tsx
@@ -1,68 +1,75 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import './Profile.css';
+import { useState } from 'react';
+import { ProfileSkeleton } from './ProfileSkeleton';
 
 export function Profile() {
+  const [isLoading, setIsLoading] = useState<Boolean>(false)//false mientras conectamos front y back
   return (
     <div className="profileContainer">
-      <div className="profileHeader">
-        <div className="profileAvatar">
-          <div className="logoPerfil"></div>
-        </div>
-        <div className="profileInfo">
-          <h2 className="username">Usuario</h2>
-          <div className="followStats">
-            <div className="stat">
-              <p className="statLabel">Seguidos</p>
-              <p className="statNumber">0</p>
+      {isLoading? <ProfileSkeleton/>:(
+        <div>
+          <div className="profileHeader">
+            <div className="profileAvatar">
+              <div className="logoPerfil"></div>
             </div>
-            <div className="stat">
-              <p className="statLabel">Seguidores</p>
-              <p className="statNumber">0</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Botones de acción */}
-      <div className="profileActions">
-        <button className="actionBtn editBtn">Editar Perfil</button>
-        <button className="actionBtn configBtn">Configurar Cuenta</button>
-        <button className="actionBtn shareBtn">
-          <FontAwesomeIcon icon={faShare}/>
-        </button>
-      </div>
-
-      {/* Sección de contenido */}
-      <div className="contentSections">
-        {/* Historial */}
-        <div className="section">
-          <h3 className="sectionTitle">Historial</h3>
-          <div className="itemsGrid">
-            {[...Array(10)].map((_, index) => (
-              <div key={index} className="item">
-                <div className="itemCover">Portada</div>
-                <div>
-                  <p className="itemName">Nombre</p>
-                  <p className="itemArtist">Artista</p>
+            <div className="profileInfo">
+              <h2 className="username">Usuario</h2>
+              <div className="followStats">
+                <div className="stat">
+                  <p className="statLabel">Seguidos</p>
+                  <p className="statNumber">0</p>
+                </div>
+                <div className="stat">
+                  <p className="statLabel">Seguidores</p>
+                  <p className="statNumber">0</p>
                 </div>
               </div>
-            ))}
+            </div>
           </div>
-        </div>
 
-        {/* Imágenes */}
-        <div className="section">
-          <h3 className="sectionTitle">Imágenes</h3>
-          <div className="itemsGrid">
-            {[...Array(10)].map((_, index) => (
-              <div key={index} className="item">
-                <div className="itemCover">Portada</div>
+          {/* Botones de acción */}
+          <div className="profileActions">
+            <button className="actionBtn editBtn">Editar Perfil</button>
+            <button className="actionBtn configBtn">Configurar Cuenta</button>
+            <button className="actionBtn shareBtn">
+              <FontAwesomeIcon icon={faShare}/>
+            </button>
+          </div>
+
+          {/* Sección de contenido */}
+          <div className="contentSections">
+            {/* Historial */}
+            <div className="section">
+              <h3 className="sectionTitle">Historial</h3>
+              <div className="itemsGrid">
+                {[...Array(10)].map((_, index) => (
+                  <div key={index} className="item">
+                    <div className="itemCover">Portada</div>
+                    <div>
+                      <p className="itemName">Nombre</p>
+                      <p className="itemArtist">Artista</p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+
+            {/* Imágenes */}
+            <div className="section">
+              <h3 className="sectionTitle">Imágenes</h3>
+              <div className="itemsGrid">
+                {[...Array(10)].map((_, index) => (
+                  <div key={index} className="item">
+                    <div className="itemCover">Portada</div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
---
+++ vibraApp/src/components/UserPage/ProfileSkeleton.tsx
@@ -0,0 +1,59 @@
+import './ProfileSkeleton.css'
+export function ProfileSkeleton() {
+  return (
+    <div className="profileContainer">
+      <div className="loadingContainer">
+        {/* Profile Header Skeleton */}
+        <div className="profileHeader">
+          <div className="profileAvatar">
+            <div className="logoPerfil skeleton skeleton-avatar"></div>
+          </div>
+          <div className="profileInfo">
+            <div className="skeleton skeleton-text skeleton-username"></div>
+            <div className="followStats">
+              <div className="stat">
+                <div className="skeleton skeleton-text skeleton-label"></div>
+                <div className="skeleton skeleton-text skeleton-number"></div>
+              </div>
+              <div className="stat">
+                <div className="skeleton skeleton-text skeleton-label"></div>
+                <div className="skeleton skeleton-text skeleton-number"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Content Sections Skeleton */}
+        <div className="contentSections">
+          {/* Historial Skeleton */}
+          <div className="section">
+            <div className="skeleton skeleton-title"></div>
+            <div className="itemsGrid">
+              {[...Array(3)].map((_, index) => (
+                <div key={index} className="item">
+                  <div className="skeleton skeleton-item-cover"></div>
+                  <div>
+                    <div className="skeleton skeleton-text skeleton-item-name"></div>
+                    <div className="skeleton skeleton-text skeleton-item-artist"></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Imágenes Skeleton */}
+          <div className="section">
+            <div className="skeleton skeleton-title"></div>
+            <div className="itemsGrid">
+              {[...Array(3)].map((_, index) => (
+                <div key={index} className="item">
+                  <div className="skeleton skeleton-item-cover"></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
